dd2afcfb1ac4d93e4903a14fc391addb5b464929
Arturo <arturcas2@hotmail.com>
2021-07-27 20:38:00 -0500

    Configure bottons and progress

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const $video = document.querySelector('#video');
 const $play = document.querySelector('#play');
 const $pause = document.querySelector('#pause');
+const $backward = document.querySelector('#backward');
+const $forward = document.querySelector('#forward');
 
 $play.addEventListener('click', handlePlay);
 $pause.addEventListener('click', handlePause);
@@ -17,4 +19,41 @@ function handlePause() {
 	$play.hidden = false;
 	$pause.hidden = true;
 	console.log('Le diste click al botón de pause');
+}
+
+$backward.addEventListener('click', handleBackward);
+
+function handleBackward() {
+	// $video.currentTime = $video.currentTime - 10;
+	$video.currentTime -= 10;
+	console.log('Retroceder 10 segundos');
+}
+
+$forward.addEventListener('click', handleForward);
+
+function handleForward() {
+	// $video.currentTime = $video.currentTime + 10
+	$video.currentTime += 10;
+	console.log('Adelantar 10 segundos', $video.currentTime);
+}
+
+$video.addEventListener('loadedmetadata', handleLoaded);
+$video.addEventListener('timeupdate', handleTimeUpdate);
+
+const $progress = document.querySelector('#progress');
+function handleLoaded() {
+	$progress.max = $video.duration;
+	console.log('Video cargado', $video.duration);
+}
+
+function handleTimeUpdate() {
+	$progress.value = $video.currentTime;
+	console.log('Tiempo actual	', $video.currentTime);
+}
+
+$progress.addEventListener('input', hadleInput);
+
+function hadleInput() {
+	$video.currentTime = $progress.value;
+	console.log($progress.value);
 }
